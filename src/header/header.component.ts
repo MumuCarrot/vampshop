55905a6fb9bf81f6851ce6b11708b0d6450664f5
MumuCarrot <ivan.kovalenko8080@gmail.com>
2025-01-17 21:40:24 +0100

    non functional header created

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: "header-comp",
     standalone: true,
+    imports: [FormsModule],
     templateUrl: "./header.component.html",
     styleUrl: "./header.component.css"
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    s:string = "";
+
+    searchFor(request: string) {
+        alert(`Searching for ${request}`);
+    }
+}
