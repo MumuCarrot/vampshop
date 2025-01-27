03503d88d9bdcb4fd26398ff227e2ada5b33f6bc
MumuCarrot <ivan.kovalenko8080@gmail.com>
2025-01-27 18:52:10 +0100

    sign in window finalized and needs to connect httpi server-side checking

--- src/app/sign.component.ts
+++ src/app/sign.component.ts
@@ -1,19 +1,26 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 import { SignService } from "./sign.service";
 
 @Component({
     selector: "sign-comp",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: "./sign.component.html",
     styleUrl: "./sign.component.css"
 })
 export class SignComponent {
     constructor(public sign: SignService) {}
 
+    login: string = "";
+
     onPopupClick(event: Event): void {
         event.stopPropagation();
+    }
+
+    signContinuation(): void {
+        console.warn("TODO")
     }
 }
